81ea1bb025b487bf36ab8fdc124557b328895bc3
Timo Tijhof <krinkle@fastmail.com>
2023-09-16 20:00:16 -0700

    Build: Fix typo in wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -22,7 +22,7 @@ define( 'WP_DEBUG', (bool) JQUERY_STAGING );
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress_' . strtr( JQUERY_LIVE_SITE, [ '.' => '_' ]);
+define('DB_NAME', 'wordpress_' . strtr( JQUERY_LIVE_SITE, [ '.' => '_' ]));
 
 /** MySQL database username */
 define('DB_USER', getenv('WORDPRESS_DB_USER'));
